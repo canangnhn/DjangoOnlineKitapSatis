f65de7304081db4454fff39e5fb203a8a69e4e94
canangnhn <53625644+canangunhan@users.noreply.github.com>
2020-05-12 22:54:52 -0700

    Kullanıcı Bilgilerini ve Şifreyi Güncelleme

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,10 @@ from user import views
 urlpatterns = [
     # ex: /blog/
     path('', views.index, name='index'),
-   # path('addcomment/<int:id>', views.addcomment, name='addcomment')
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.change_password, name='change_password'),
+    path('orders/', views.orders, name='orders'),
+
 
 
 
